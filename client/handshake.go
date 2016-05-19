8161b56e6b5426ddfcd76bf9460a31347d872156
Julian Kornberger <jk+github@digineo.de>
2016-05-19 15:37:14 +0200

    Handshake [WIP]

--- client/handshake.go
+++ client/handshake.go
@@ -5,76 +5,85 @@ import (
 )
 
 func handlePacket(msg *Message) (reply *Message) {
-	records := msg.Records
-	val := records[RECORD_HANDSHAKE_TYPE]
+	val := msg.Records[RECORD_HANDSHAKE_TYPE]
 	if len(val) != 1 {
 		return
 	}
 
-	t := val[0]
+	//for k, v := range records {
+	//	log.Println(k, v)
+	//}
 
+	t := val[0]
 	switch t {
 	case 1:
-		// Handshake request
-		reply = msg.NewReply()
-		for k, v := range records {
-			log.Println(k, v)
-		}
-
-		senderKey := records[RECORD_SENDER_KEY]
-		recipientKey := records[RECORD_RECIPIENT_KEY]
-		senderHandshakeKey := records[RECORD_SENDER_HANDSHAKE_KEY]
-
-		if senderKey == nil {
-			log.Println("sender key missing")
-			reply.SetError(REPLY_RECORD_MISSING, RECORD_SENDER_KEY)
-			return
-		}
-		if recipientKey == nil {
-			log.Println("recipient key missing")
-			reply.SetError(REPLY_RECORD_MISSING, RECORD_RECIPIENT_KEY)
-			return
-		}
-		if senderHandshakeKey == nil {
-			log.Println("sender handshake key missing")
-			reply.SetError(REPLY_RECORD_MISSING, RECORD_SENDER_HANDSHAKE_KEY)
-			return
-		}
-
-		log.Printf("received handshake from %s[%d] using fastd %s", msg.Src.IP.String(), msg.Src.Port, records[RECORD_VERSION_NAME])
-
-		// TODO check recipientKey
-		// TODO check timeout
-		reply.Records[RECORD_REPLY_CODE] = []byte{REPLY_SUCCESS}
-		reply.Records[RECORD_METHOD_LIST] = []byte("null")
-		reply.Records[RECORD_VERSION_NAME] = []byte("v18")
-		reply.Records[RECORD_MTU] = records[RECORD_MTU]
-		reply.Records[RECORD_SENDER_KEY] = recipientKey
-		reply.Records[RECORD_RECIPIENT_KEY] = senderKey
-		reply.Records[RECORD_RECIPIENT_HANDSHAKE_KEY] = senderHandshakeKey
-	case 3:
-		reply = msg.NewReply()
-
-		// Handshake finish
-		methodName := msg.Records[RECORD_METHOD_NAME]
-		if methodName == nil {
-			log.Println("method name missing")
-			reply.SetError(REPLY_RECORD_MISSING, RECORD_METHOD_NAME)
-			return
-		}
-		if string(methodName) != "null" {
-			log.Println("method name invalid:", methodName)
-			reply.SetError(REPLY_UNACCEPTABLE_VALUE, RECORD_METHOD_NAME)
-			return
-		}
-
-		reply.Records[RECORD_REPLY_CODE] = []byte{REPLY_SUCCESS}
-
+		reply = respondHandshake(msg)
 		// TODO
-
+	case 3:
+		reply = handleFinishHandshake(msg)
 	default:
 		log.Printf("unsupported handshake type: %d", t)
 	}
 
 	return
+}
+
+// Handshake request
+func respondHandshake(msg *Message) (reply *Message) {
+	records := msg.Records
+	reply = msg.NewReply()
+
+	senderKey := records[RECORD_SENDER_KEY]
+	recipientKey := records[RECORD_RECIPIENT_KEY]
+	senderHandshakeKey := records[RECORD_SENDER_HANDSHAKE_KEY]
+
+	if senderKey == nil {
+		log.Println("sender key missing")
+		reply.SetError(REPLY_RECORD_MISSING, RECORD_SENDER_KEY)
+		return
+	}
+	if recipientKey == nil {
+		log.Println("recipient key missing")
+		reply.SetError(REPLY_RECORD_MISSING, RECORD_RECIPIENT_KEY)
+		return
+	}
+	if senderHandshakeKey == nil {
+		log.Println("sender handshake key missing")
+		reply.SetError(REPLY_RECORD_MISSING, RECORD_SENDER_HANDSHAKE_KEY)
+		return
+	}
+
+	log.Printf("received handshake from %s[%d] using fastd %s", msg.Src.IP.String(), msg.Src.Port, records[RECORD_VERSION_NAME])
+
+	// TODO check recipientKey
+	// TODO check timeout
+	reply.Records[RECORD_REPLY_CODE] = []byte{REPLY_SUCCESS}
+	reply.Records[RECORD_METHOD_LIST] = []byte("null")
+	reply.Records[RECORD_VERSION_NAME] = []byte("v18")
+	reply.Records[RECORD_MTU] = records[RECORD_MTU]
+	reply.Records[RECORD_SENDER_KEY] = recipientKey
+	reply.Records[RECORD_RECIPIENT_KEY] = senderKey
+	reply.Records[RECORD_RECIPIENT_HANDSHAKE_KEY] = senderHandshakeKey
+
+	return reply
+}
+
+func handleFinishHandshake(msg *Message) (reply *Message) {
+	reply = msg.NewReply()
+
+	// Handshake finish
+	methodName := msg.Records[RECORD_METHOD_NAME]
+	if methodName == nil {
+		log.Println("method name missing")
+		reply.SetError(REPLY_RECORD_MISSING, RECORD_METHOD_NAME)
+		return
+	}
+	if string(methodName) != "null" {
+		log.Println("method name invalid:", methodName)
+		reply.SetError(REPLY_UNACCEPTABLE_VALUE, RECORD_METHOD_NAME)
+		return
+	}
+
+	reply.Records[RECORD_REPLY_CODE] = []byte{REPLY_SUCCESS}
+	return reply
 }
--- client/libuecc_test.go
+++ client/libuecc_test.go
@@ -7,15 +7,12 @@ import (
 )
 
 func TestGetPublic(t *testing.T) {
-	secretHex := "c05b5c69be567ff1f82598707096bbc63b04a77974f88c334a0d38065180bc7d"
-	publicHex := "f9adea6eb454cc8540266b114e21881b917ee120f307a457e96c30c5935b9485"
 	assert := assert.New(t)
 
-	secret, err := hex.DecodeString(secretHex)
-	assert.NoError(err)
-	assert.NotEmpty(secret)
+	secret := "c05b5c69be567ff1f82598707096bbc63b04a77974f88c334a0d38065180bc7d"
+	public := "f9adea6eb454cc8540266b114e21881b917ee120f307a457e96c30c5935b9485"
 
-	assert.Equal(publicHex, hex.EncodeToString(GetPublic(secret)))
+	assert.Equal(public, hex.EncodeToString(GetPublic(MustDecodeString(secret))))
 }
 
 func TestMakeSharedHandshakeKey(t *testing.T) {
@@ -26,4 +23,12 @@ func TestMakeSharedHandshakeKey(t *testing.T) {
 	assert.NoError(err)
 	assert.NotEmpty(handshakeKey)
 	assert.True(makeSharedHandshakeKey(false, handshakeKey))
+}
+
+func MustDecodeString(str string) []byte {
+	decoded, err := hex.DecodeString(str)
+	if err != nil {
+		panic(err)
+	}
+	return decoded
 }
