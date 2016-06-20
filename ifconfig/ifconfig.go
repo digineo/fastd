dcb12a65aa235dee2a4629459ae73bda64309785
Julian Kornberger <jk+github@digineo.de>
2016-06-20 19:02:39 +0200

    ifconfig: Use To4() to detect address family
    https://groups.google.com/d/msg/golang-nuts/DJxwSccz2Bs/euGu9hkGDAAJ

--- ifconfig/ifconfig.go
+++ ifconfig/ifconfig.go
@@ -5,14 +5,5 @@ import (
 )
 
 func IsIPv4(ip net.IP) bool {
-	return len(ip) == net.IPv4len || (len(ip) > 11 && isZeros(ip[0:10]) && ip[10] == 0xff && ip[11] == 0xff)
-}
-
-func isZeros(ip net.IP) bool {
-	for _, b := range ip {
-		if b != 0 {
-			return false
-		}
-	}
-	return true
+	return ip.To4() != nil
 }
--- ifconfig/sockaddr_freebsd.go
+++ ifconfig/sockaddr_freebsd.go
@@ -10,19 +10,21 @@ import (
 	Returns a struct sockaddr_storage* that is in fact a sockaddr_in* or sockaddr_in6*
 */
 func Sockaddr(addr net.IP) *syscall.RawSockaddrAny {
-	if IsIPv4(addr) {
+	if bytes := addr.To4(); bytes != nil {
 		raw := syscall.RawSockaddrInet4{
 			Len:    syscall.SizeofSockaddrInet4,
 			Family: syscall.AF_INET,
 		}
-		copy(raw.Addr[:], addr.To4())
+		copy(raw.Addr[:], bytes)
 		return (*syscall.RawSockaddrAny)(unsafe.Pointer(&raw))
-	} else {
+	} else if bytes := addr.To16(); bytes != nil {
 		raw := syscall.RawSockaddrInet6{
 			Len:    syscall.SizeofSockaddrInet6,
 			Family: syscall.AF_INET6,
 		}
-		copy(raw.Addr[:], addr.To16())
+		copy(raw.Addr[:], bytes)
 		return (*syscall.RawSockaddrAny)(unsafe.Pointer(&raw))
+	} else {
+		return nil
 	}
 }
