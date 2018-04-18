1a44696b3959f28e7fe003b296565ea11a5cb03d
Dominik Menke <dom@digineo.de>
2018-04-18 18:09:12 +0200

    client: Interface implementation for Linux [WIP]

---
+++ cmd/client/tun_linux.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/songgao/water"
+	"github.com/vishvananda/netlink"
+)
+
+type linuxTunIface struct {
+	iface *water.Interface
+}
+
+var _ Interface = (*linuxTunIface)(nil)
+
+func (tun *linuxTunIface) Close() error {
+	return tun.iface.Close()
+}
+
+func (tun *linuxTunIface) Name() string {
+	return tun.iface.Name()
+}
+
+func (tun *linuxTunIface) Configure(local, remote net.IP, mtu uint16) error {
+	link, err := netlink.LinkByName(tun.iface.Name())
+	if err != nil {
+		return err
+	}
+
+	netlink.LinkSetMTU(link, int(mtu))
+
+	log.Printf("link: %#v", link)
+	log.Printf("link attrs: %#v", link.Attrs())
+
+	return nil // fmt.Errorf("not implemented yet")
+}
+
+func newTunDevice() (Interface, error) {
+	config := water.Config{DeviceType: water.TUN}
+
+	if name, err := findName("tun"); err == nil {
+		config.Name = name
+	} else {
+		return nil, err
+	}
+
+	iface, err := water.New(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &linuxTunIface{iface: iface}, nil
+}
+
+func findName(prefix string) (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	samePrefix := make(map[string]struct{})
+	mark := struct{}{}
+
+	for _, iface := range ifaces {
+		if strings.HasPrefix(iface.Name, prefix) {
+			samePrefix[strings.TrimPrefix(iface.Name, prefix)] = mark
+		}
+	}
+
+	var name string
+	for id := 0; ; id++ {
+		name = fmt.Sprintf("%s%d", prefix, id)
+		if _, exists := samePrefix[name]; !exists {
+			break
+		}
+	}
+	return name, nil
+}
