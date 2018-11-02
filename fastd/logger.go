6542de9cf0f5389ac9ec19e527d3dcc8eaa49a1a
Dominik Menke <dom@digineo.de>
2018-11-02 16:07:19 +0100

    fastd: tests for logger
    Having log not export its default logger instance makes mocking the
    standard log package a wee bit complex.

--- fastd/logger.go
+++ fastd/logger.go
@@ -1,6 +1,9 @@
 package fastd
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 // Logger defines log methods used by this package.
 type Logger interface {
@@ -20,8 +23,22 @@ func SetLogger(l Logger) {
 	}
 }
 
-// stdlogLogger is a thin wrapper around the standard log package.
-type stdlogLogger struct{}
+// log.std is not exposed, so we need to trick a bit to make tests working
+type stdlogOutput func(int, string) error
 
-func (l *stdlogLogger) Infof(format string, a ...interface{})  { log.Printf("INFO - "+format, a...) }
-func (l *stdlogLogger) Errorf(format string, a ...interface{}) { log.Printf("ERROR - "+format, a...) }
+// stdlogLogger is a thin wrapper around the standard log package.
+type stdlogLogger struct {
+	o stdlogOutput // set in tests
+}
+
+func (l *stdlogLogger) Infof(format string, a ...interface{})  { l.out("INFO", format, a...) }
+func (l *stdlogLogger) Errorf(format string, a ...interface{}) { l.out("ERROR", format, a...) }
+
+func (l *stdlogLogger) out(level, format string, a ...interface{}) {
+	msg := fmt.Sprintf(level+" - "+format, a...)
+	if l.o == nil {
+		log.Output(3, msg)
+		return
+	}
+	l.o(4, msg)
+}
